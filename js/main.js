b9f8c381c23d10ed321bac7bc9c7d4a6f4f71ee6
didozavar <didozavar@gmail.com>
2015-01-18 04:01:06 -0800

    Survey fix

--- js/main.js
+++ js/main.js
@@ -4,37 +4,7 @@ $(document).ready(function(){
         $(".group1").colorbox({rel:'group1'});
     });
 
-    //survey code
-    function vote(){
-        var myRadioButtons = document.getElementsByName('survey');
-        var elements = myRadioButtons.length;
 
-        for(var i = 1; i <= elements; i++)
-        {
-            var index = i.toString();
-            var choice = document.getElementById(index);
-            if(choice.checked){
-                index = '.'.concat(index);
-                var boxer = $(index).text();
-                var val = choice.value;
-                $.ajax({
-                    dataType: "json",
-                    url: 'json/survey.json',
-                    success: function(data){
-                        $.each(data,function(index, value){
-                            if(index == val)
-                            {
-                                var newresult = parseInt(value)+1;
-                                var text = "Вие гласувахте за ".concat(boxer, "! ", "Общо гласували за ",boxer,": ", newresult);
-                                alert(text);
-                            }
-                        });
-                    }
-                });
-                //alert(result);
-            }
-        }
-    }
 
     //submenu code
     $('.navigation ul > li').bind('mouseover', openSubMenu);
@@ -59,3 +29,35 @@ $(document).ready(function(){
         alert("Споделено чрез ".concat(name));
     });
 });
+
+//survey code
+function vote(){
+    var myRadioButtons = document.getElementsByName('survey');
+    var elements = myRadioButtons.length;
+
+    for(var i = 1; i <= elements; i++)
+    {
+        var index = i.toString();
+        var choice = document.getElementById(index);
+        if(choice.checked){
+            index = '.'.concat(index);
+            var boxer = $(index).text();
+            var val = choice.value;
+            $.ajax({
+                dataType: "json",
+                url: 'json/survey.json',
+                success: function(data){
+                    $.each(data,function(index, value){
+                        if(index == val)
+                        {
+                            var newresult = parseInt(value)+1;
+                            var text = "Вие гласувахте за ".concat(boxer, "! ", "Общо гласували за ",boxer,": ", newresult);
+                            alert(text);
+                        }
+                    });
+                }
+            });
+            //alert(result);
+        }
+    }
+}
