c2dfba74a610b477890ac17165e2c78b06ce8591
didozavar <didozavar@gmail.com>
2015-01-18 03:38:42 -0800

    Refactore code

---
+++ js/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+    //Gallery code
+    $(document).ready(function(){
+        $(".group1").colorbox({rel:'group1'});
+    });
+
+    //survey code
+    function vote(){
+        var myRadioButtons = document.getElementsByName('survey');
+        var elements = myRadioButtons.length;
+
+        for(var i = 1; i <= elements; i++)
+        {
+            var index = i.toString();
+            var choice = document.getElementById(index);
+            if(choice.checked){
+                index = '.'.concat(index);
+                var boxer = $(index).text();
+                var val = choice.value;
+                $.ajax({
+                    dataType: "json",
+                    url: 'json/survey.json',
+                    success: function(data){
+                        $.each(data,function(index, value){
+                            if(index == val)
+                            {
+                                var newresult = parseInt(value)+1;
+                                var text = "Вие гласувахте за ".concat(boxer, "! ", "Общо гласували за ",boxer,": ", newresult);
+                                alert(text);
+                            }
+                        });
+                    }
+                });
+                //alert(result);
+            }
+        }
+    }
+
+    //submenu code
+    $('.navigation ul > li').bind('mouseover', openSubMenu);
+    $('.navigation ul > li').bind('mouseout', closeSubMenu);
+
+    function openSubMenu() {
+        if($('.mobile_nav').css('visibility') == 'hidden') {
+            $(this).find('ul').css('visibility', 'visible');
+        }
+    };
+
+    function closeSubMenu() {
+        if($('.mobile_nav').css('visibility') == 'hidden') {
+            $(this).find('ul').css('visibility', 'hidden');
+        }
+    };
+
+    //share code
+
+    $('.share').click(function(){
+        var name = $(this).attr('name');
+        alert("Споделено чрез ".concat(name));
+    });
+});
--- js/share.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$('.share').click(function(){
-		var name = $(this).attr('name');
-		alert("Споделено чрез ".concat(name));
-	});
-});
